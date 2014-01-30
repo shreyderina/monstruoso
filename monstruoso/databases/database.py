117fb37ac8fdb273c1aa90685b4d6ea984394afe
Шрейдер Ирина Сергеевна <i.shreyder@uk-rnd-114.(none)>
2014-01-30 11:13:15 +0700

    few changes in db

--- monstruoso/databases/database.py
+++ monstruoso/databases/database.py
@@ -1,5 +1,7 @@
 import psycopg2
+import redis
 from pymongo import MongoClient
+from psycopg2.extras import RealDictCursor
 
 class Database(object):
 
@@ -25,7 +27,12 @@ class Postgres(Database):
     def __init__(self, db_host, db_port, db_name, db_user, db_password):
         self.connection = psycopg2.connect(host=db_host, port=db_port, database=db_name, user=db_user, password=db_password)
 
+    def __del__(self):
+        self.connection.commit()
+        self.connection.close()
+
     def command(self, psql):
+        self.connection.cursor_factory = RealDictCursor
         self.cursor = self.connection.cursor()
         self.cursor.execute(psql)
 
@@ -36,6 +43,9 @@ class Postgres(Database):
     def select_all(self, query):
         self.command(query)
         return self.cursor.fetchall()
+
+    def insert_row(self, query):
+        self.command(query)
 
 
 class Mongo(Database):
@@ -53,6 +63,17 @@ class Mongo(Database):
         collection = db.all_messages
         return list(collection.find(query))
 
+class Redis(Database):
+
+    def __init__(self, db_host):
+        self.connection = redis.StrictRedis(db_host)
+
+    def get_keys(self):
+        return self.connection.keys()
+
+    def get_keys_by_pattern(self, pattern):
+        return self.connection.keys(pattern)
+
 class Connection(object):
 
     def create_db_connection(self, type, host, port = None, user = None, password = None, db_name = None):
@@ -60,5 +81,7 @@ class Connection(object):
             return Postgres(host, port, db_name, user, password)
         if type == 'mongo':
             return Mongo(host)
+        if type == 'redis':
+            return Redis(host)
         else:
             pass
